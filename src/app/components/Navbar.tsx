ca1f71346b3251891ff2fec626c385a4ee705ac4
kavelogics-tech <work@kavelogics.com>
2025-08-02 15:57:10 +0500

    set variabls globally

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <div className='bg-red-400'>WorkPhotos</div>
+
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/app/components/homeComponents/HeroSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div className='h-screen bg-primary'> Home Hero Section</div>
+  )
+}
+
+export default HeroSection
